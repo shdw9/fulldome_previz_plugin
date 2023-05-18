6dd70e285784e8d2ed8b89823c042da071b7e4c4
CK <73201351+shdw9@users.noreply.github.com>
2023-05-18 15:32:31 -0700

    added fdp

---
+++ Packages/fulldome-previz-plugin/Editor/Scripts/PrevizPlugin.cs
@@ -0,0 +1,83 @@
+using System.Collections.Generic;
+using System.Linq;
+using System.IO;
+using System;
+using Newtonsoft.Json.Linq;
+using Newtonsoft.Json;
+
+using UnityEditor;
+using UnityEngine;
+using UnityEngine.UIElements;
+using UnityEngine.Playables;
+using UnityEngine.Timeline;
+using UnityEditor.PackageManager;
+
+using Unity.Formats.USD;
+
+public class PrevizPlugin : MonoBehaviour
+{
+    public static GameObject usdScene;
+
+    // modified version of MenuImportAsGameObjects()
+    // from com.unity.formats.usd/Editor/Scripts/Behaviors/UsdMenu.cs
+    [MenuItem("USD/Import with Timeline Clip", priority = 4)]
+    public static void ImportWithTimeline()
+    {
+        // import usd file
+        var scene = Unity.Formats.USD.ImportHelpers.InitForOpen();
+        if (scene == null)
+        {
+            return;
+        }
+
+        var usd = Unity.Formats.USD.ImportHelpers.ImportSceneAsGameObject(scene, Selection.activeGameObject);
+        scene.Close();
+
+        // create timeline and playable director
+        usdScene = GameObject.Find(usd.name);
+        var timeline = ScriptableObject.CreateInstance<TimelineAsset>();
+        usdScene.AddComponent<PlayableDirector>();
+        usdScene.GetComponent<PlayableDirector>().playableAsset = timeline;
+
+        var usdPlayable = timeline.CreateTrack<UsdPlayableTrack>(null, "");
+        usdScene.GetComponent<PlayableDirector>().SetGenericBinding(usdPlayable, usdScene.GetComponent<UsdAsset>());
+
+        var usdPlayableClip = usdPlayable.CreateDefaultClip();
+        usdPlayableClip.duration = usdScene.GetComponent<Unity.Formats.USD.UsdAsset>().Length;
+        var usdPlayableAsset = usdPlayableClip.asset as UsdPlayableAsset;
+
+        usdPlayableAsset.SourceUsdAsset = new ExposedReference<UsdAsset> { exposedName = Guid.NewGuid().ToString() };
+        usdScene.GetComponent<PlayableDirector>().SetReferenceValue(usdPlayableAsset.SourceUsdAsset.exposedName, usdScene.GetComponent<UsdAsset>());
+
+        Debug.Log("[PREVIZ] Successfully imported" + usd.name + " with a timeline clip length of " + usdPlayableClip.duration + " seconds");
+    }
+
+    [MenuItem("USD/Export Scene with Unity Recorder", priority = 50)]
+    public static void OpenRecorderWindow()
+    {
+        // Open the Unity Recorder Window To Export USD
+        EditorWindow window = EditorWindow.GetWindow(typeof(UnityEditor.Recorder.RecorderWindow));
+        window.Show();
+        window.Focus();
+    }
+
+    [MenuItem("USD/Previzualization Panels/Animation Window", priority = 160)]
+    public static void AnimatorWindow()
+    {
+        System.Type windowType = typeof(UnityEditor.Editor).Assembly.GetType("UnityEditor.AnimationWindow");
+        EditorWindow window = EditorWindow.GetWindow(windowType);
+        window.Show();
+        window.Focus();
+    }
+
+    [MenuItem("USD/Previzualization Panels/USD Recorder Window", priority = 190)]
+    public static void RecorderWindow()
+    {
+        EditorWindow window = EditorWindow.GetWindow(typeof(UnityEditor.Recorder.RecorderWindow));
+        window.Show();
+        window.Focus();
+    }
+
+    
+
+}
---
+++ Packages/fulldome-previz-plugin/Editor/Scripts/Timeline/ExportTimeline.cs
@@ -0,0 +1,57 @@
+using System.Collections.Generic;
+using System.Linq;
+using System.IO;
+using System;
+using Newtonsoft.Json.Linq;
+using Newtonsoft.Json;
+
+using UnityEditor;
+using UnityEngine;
+using UnityEngine.UIElements;
+using UnityEngine.Playables;
+using UnityEngine.Timeline;
+using UnityEditor.PackageManager;
+
+using Unity.Formats.USD;
+public class ExportTimeline : MonoBehaviour
+{
+    [MenuItem("USD/Timeline/Export Timeline Information", priority = 190)]
+    public static void ExportToJson()
+    {
+        if (PrevizPlugin.usdScene == null) {
+            Debug.Log("[PREVIZ] Cannot find the imported USD scene! Try importing a USD scene via \"Import with Timeline Clip\"!");
+            return;
+        }
+
+        var timeline = (TimelineAsset) PrevizPlugin.usdScene.GetComponent<PlayableDirector>().playableAsset;
+        string timelineJson = "{\"" + PrevizPlugin.usdScene.name + "\": {";
+        
+        timelineJson += "\"usdPath\": \"" + PrevizPlugin.usdScene.GetComponent<Unity.Formats.USD.UsdAsset>().usdFullPath.Replace("\\","/") + "\", ";
+        foreach (TrackAsset track in timeline.GetOutputTracks())
+        {
+            timelineJson += "\"" + track.ToString().Split('(', ')')[1] + "\": {";
+            timelineJson += "\"trackName\": \"" + track.name + "\", ";
+
+            timelineJson += "\"clipInfo\": ";
+            timelineJson += JsonUtility.ToJson(track);
+
+            timelineJson += "}}";
+            
+        }
+        timelineJson += "}";
+
+        var path = EditorUtility.SaveFilePanel(
+            "[FDP] Save timeline information as JSON",
+            "",
+            "fdp-timeline.json",
+            "json");
+
+        if (path.Length != 0)
+        {
+            Debug.Log(timelineJson);
+            File.WriteAllText(path, JsonConvert.SerializeObject(JObject.Parse(timelineJson), Formatting.Indented));
+            Debug.Log("[PREVIZ] Saved timeline information to " + path + "!");
+        }
+
+    }
+}
