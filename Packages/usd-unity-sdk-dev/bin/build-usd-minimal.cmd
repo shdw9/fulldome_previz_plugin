06eab4e6f2bbfc2d97dd213d8d46f2c6ce2327c9
CK <73201351+shdw9@users.noreply.github.com>
2023-05-18 23:47:34 -0700

    removed all unity scene files, kept only package files

--- Packages/usd-unity-sdk-dev/bin/build-usd-minimal.cmd
+++
@@ -1,1 +0,0 @@
-python build_scripts\build_usd.py --build-monolithic --alembic --no-python --no-imaging --no-examples --no-tutorials ..\_build-monolithic\
--- Packages/usd-unity-sdk-dev/bin/build-usd-python.cmd
+++
@@ -1,1 +0,0 @@
-python build_scripts\build_usd.py --build-monolithic --openimageio --alembic ..\_build-monolithic-py\
